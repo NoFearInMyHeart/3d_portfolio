02c54ce9cff8fb0a4615ee0ec9fc21eaab96eb61
zYz <zhuyueze>
2023-04-06 21:34:52 +0800

    add remove script

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -2,7 +2,6 @@ import {Suspense, useEffect, useState} from "react";
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
 import CanvasLoader from "../Loader";
-import * as querystring from "querystring";
 
 interface ComputersPropsItl {
     isMobile: boolean
