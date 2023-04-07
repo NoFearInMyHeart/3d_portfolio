4e859c8bb29cd8af3047ac08c3b0bc81f82de903
zYz <zhuyueze>
2023-04-07 09:12:34 +0800

    redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['react-tilt'],
+  async redirects() {
+    return [
+      {
+        source: '/project',
+        destination: '/#projects',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
